2b74fd2e0f534ce3ada0518e38fe3138a3eb6e72
Wallace Tácio <wallacett.dev@gmail.com>
2021-08-26 20:19:29 -0300

    Adiciona resolução do exercício Meme Tô Nervoso ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resposta;
+
+            Console.Write("Usuário o que você pensa sobre mim? ");
+            resposta = Console.ReadLine().Remove(10);
+
+            Console.Write("- E sabe o que eu penso de você USUÁRIO?\n {0}... ",resposta);
+            Console.WriteLine("NÃO, PERA.\n Tô nervoso");
+        }
+            
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # MemeToNervoso
 Exercício Meme tô Nervoso ETEC 2021
+
+**Enunciado**:
+
+Peça ao usuário que ele digite o que pensa sobre esse programa. Exiba uma resposta em versão meme no seguinte formato:
+
+```
+Usuário, o que você pensa sobre mim? Você é muito sem graça!
+
+- E sabe o que eu penso de você?
+Você é mui... NÃO, PERA.
+Tô nervoso
+```
+**Como executar**
+
+Acesse a pasta do projeto e digite:
+
+```
+dotnet run
+```
